cd38d2c9284c7e6e1d8f70bb9b7ba84522e1e2d4
Exillite <greencod67@gmail.com>
2022-01-07 22:54:55 +0300

    Start
    API for cliens is ready on 50%. Student part is ready, but teacher part is not.
    Full ready login/regestration page.

---
+++ serv.py
@@ -0,0 +1,298 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import json
+
+
+import smtplib                                      # Импортируем библиотеку по работе с SMTP
+
+# Добавляем необходимые подклассы - MIME-типы
+from email.mime.multipart import MIMEMultipart      # Многокомпонентный объект
+from email.mime.text import MIMEText                # Текст/HTML
+from email.mime.image import MIMEImage              # Изображения
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///glow.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+myip = "http://127.0.0.1:5000"
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    is_active = db.Column(db.Boolean)
+    name = db.Column(db.String(50))
+    surname = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    is_teacher = db.Column(db.Boolean, default=False)
+    token = db.Column(db.String(800), nullable=False)
+ 
+    def __repr__(self):
+        return f"<users {self.id}>"
+
+class Courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(50))
+    author = db.Column(db.String(50))
+ 
+    def __repr__(self):
+        return f"<course {self.id}>"
+
+class Moduls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer)
+    number = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+ 
+    def __repr__(self):
+        return f"<module {self.id}>"
+
+class Blocks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    module_id = db.Column(db.Integer)
+    number = db.Column(db.Integer)
+    type = db.Column(db.String(50))
+    text = db.Column(db.Text)
+    condition = db.Column(db.Text)
+    answer_type = db.Column(db.String(50))
+    one_answer = db.Column(db.Boolean)
+    answers = db.Column(db.String(1000))
+    example_in = db.Column(db.String(250))
+    example_out = db.Column(db.String(250))
+ 
+    def __repr__(self):
+        return f"<users {self.id}>"
+
+class User2Course(db.Model):
+    user_id = db.Column(db.Integer)
+    course_id = db.Column(db.Integer)
+
+
+
+
+def testemail(email, tok):
+    addr_from = "noreply@exillite.xyz"                 # Адресат
+    addr_to   = email                   # Получатель
+    password  = "16188_sasa"                                  # Пароль
+
+    msg = MIMEMultipart()                               # Создаем сообщение
+    msg['From']    = addr_from                          # Адресат
+    msg['To']      = addr_to                            # Получатель
+    msg['Subject'] = 'Тема сообщения'                   # Тема сообщения
+    
+    html = """
+<html>
+<head>
+<style type="text/css">
+body{
+background: #C4DFE6;
+}
+   .a {
+    width: 690px;
+    border-radius: 5px;
+    background: #66A5AD;
+    box-shadow: 0 0 20px 9px #07575B; 
+margin-left: auto;
+    margin-right: auto;
+   }
+   .btn{
+position: relative;
+left: 50%;
+transform: translate(-50%, 0);
+height: 80px;
+font-size:18px;
+    width: 200px;
+border-radius: 5px;
+background-color: #C4DFE6;
+
+   }
+  </style>
+</head>
+  <body>
+    <div class="a">
+       <font size="5" color="black" face="sans-serif">Спасибо за регистрацию на образовательной системе <b>Glow</b>.<br>
+Чтобы проолжить регистрацию нажмите на ссылку ниже.
+</font>
+    </div>
+<br>
+<a class="btn" href=""" + f"{myip}/con/{tok}" + """>Потвердить e-mail</a>
+  </body>
+</html>
+"""
+    msg.attach(MIMEText(html, 'html', 'utf-8'))         # Добавляем в сообщение HTML-фрагмент
+
+    server = smtplib.SMTP('mail.exillite.xyz', 587)           # Создаем объект SMTP
+    server.set_debuglevel(True)                         # Включаем режим отладки - если отчет не нужен, строку можно закомментировать
+    server.starttls()                                   # Начинаем шифрованный обмен по TLS
+    server.login(addr_from, password)                   # Получаем доступ
+    server.send_message(msg)                            # Отправляем сообщение
+    server.quit()          
+
+
+@app.errorhandler(404)
+def pageNot(error):
+    return render_template('404.html', nb=True)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Log In')
+
+
+@app.route("/alreg")
+def alreg():
+    return render_template('alreg.html')
+
+
+@app.route("/al")
+def alert():
+    return render_template('testcoockies.html', nb=True, is_logined=True)
+
+
+@app.route("/con/<tok>")
+def con(tok):
+    f = Users.query.filter_by(token = tok).first()
+    f.is_active = True
+    db.session.commit()
+    return render_template('alreg.html', emconf=True)
+
+
+@app.route("/cr")
+def cr():
+    db.create_all()
+    return 'OK'
+
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    j = request.json
+
+    if j['method'] == 'reg':
+        try:
+            if len(Users.query.filter_by(email = j['email']).all()) != 0:
+                return 'ALR'
+            pshash = generate_password_hash(j['password'])
+            token = generate_password_hash(j['email'])
+            u = Users(is_active=False, name=j['name'], surname=j['surname'], email=j['email'], password=pshash, token=token)
+            db.session.add(u)
+            db.session.flush()
+            db.session.commit()
+            testemail(j['email'], token)
+            return 'OK'
+        except Exception as e:
+            print('!!>>>>' + str(e))
+            db.session.rollback()
+            return 'ERROR'
+
+    elif j['method'] == 'login':
+        try:
+            if len(Users.query.filter_by(email = j['email']).all()) == 0:
+                return 'NO'
+            u = Users.query.filter_by(email = j['email']).first()
+
+            if check_password_hash(u.password, j['password']):
+                if j['is_active']:
+                    if j['stay']:
+                        pass
+                    return 'id=' + u.token
+                else:
+                    return 'NOTACTIVE'
+            else:
+                return 'PAS'
+
+
+        except Exception as e:
+            print('!!>>>>' + str(e))
+            db.session.rollback()
+            return 'ERROR'
+
+
+    elif j['method'] == 'get_courses':
+        r = {}
+        try:
+            student_id = Users.query.filter_by(token = j['token']).first().id
+            r['token'] = j['token']
+            courses_ids = User2Course.query.filter_by(user_id = student_id).all()
+            crs = []
+            for i in courses_ids:
+                c = Courses.query.filter_by(id = i.id).first()
+                dic = {'id': c.id, 'name': c.name, 'description': c.description, 'author': c.author}
+                crs.append(dic)
+            r['count'] = len(crs)
+            r['courses'] = crs
+            r['status'] = 'OK'
+
+        except Exception as e:
+            print('!!>>>>' + str(e))
+            db.session.rollback()
+            r['status'] = 'ERROR'
+        
+        return json.dumps(r)
+
+
+
+    elif j['method'] == 'get_moduls':
+        r = {}
+        try:
+            mdls = Moduls.query.filter_by(course_id = j['course_id']).all()
+            md = []
+
+            for m in mdls:
+                dic = {'id': m.id, 'number': m.number, 'name': m.name}
+                md.append(dic)
+
+            r['count'] = len(md)
+            r['maduls'] = md
+            r['status'] = 'OK'
+
+        except Exception as e:
+            print('!!>>>>' + str(e))
+            db.session.rollback()
+            r['status'] = 'ERROR'
+        
+        return json.dumps(r)
+
+
+    elif j['method'] == 'get_blocks':
+        r = {}
+        try:
+            blocks = Blocks.query.filter_by(module_id = j['module_id']).all()
+            bl = []
+
+            for b in blocks:
+                dic = {'id': b.id, 'number': b.number, 'type': b.type}
+
+                if b.type == 'tutorial':
+                    dic['text'] = b.text
+                elif b.type == 'task':
+                    dic['condition'] = b.condition
+                    dic['answer_type'] = b.answer_type
+                elif b.type == 'task_with_answers':
+                    dic['condition'] = b.condition
+                    dic['one_answer'] = b.one_answer
+                    dic['answers'] = b.answers
+                elif b.type == 'code_task':
+                    dic['condition'] = b.condition
+                    dic['example_in'] = b.example_in
+                    dic['example_out'] = b.example_out
+
+                bl.append(dic)
+
+            r['count'] = len(bl)
+            r['blocks'] = bl
+
+            r['status'] = 'OK'
+
+        except Exception as e:
+            print('!!>>>>' + str(e))
+            db.session.rollback()
+            r['status'] = 'ERROR'
+        
+        return json.dumps(r)
+
+
+app.run(debug=True)
---
+++ static/js/login.js
@@ -0,0 +1,174 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+var fbox = document.getElementById("formbox");
+
+
+var rbtn = document.getElementById('rbtn');
+
+
+function register(){
+	x.style.left = '-400px';
+	y.style.left = '50px';
+	z.style.left = '110px';
+	fbox.style.height = '580px';
+} 
+
+function login(){
+	x.style.left = '50px';
+	y.style.left = '450px';
+	z.style.left = '0px';
+	fbox.style.height = '480px';
+}
+
+function vE(email) {
+  if (email == null) {
+    return false;
+  }
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+
+function sendReg () {
+
+  let name = document.querySelector('#rname');
+  let surname = document.querySelector('#rsurname');
+  let email = document.querySelector('#remail');
+  let password = document.querySelector('#rpassword');
+  let passwordt = document.querySelector('#rpasswordtoo');
+  var checkbox = document.getElementById('rcheckbox');  
+
+
+  if (password.value.length >= 8 && password.value == passwordt.value && checkbox.checked && name.value.length >= 2 && surname.value.length >= 3 && vE(email.value)){
+    // а вот сюда мы поместим ответ от сервера
+    let result = document.querySelector('.result');
+    // создаём новый экземпляр запроса XHR
+    let xhr = new XMLHttpRequest();
+    // адрес, куда мы отправим нашу JSON-строку
+    let url = myip + "/api";
+    // открываем соединение
+    xhr.open("POST", url, true);
+    // устанавливаем заголовок — выбираем тип контента, который отправится на сервер, в нашем случае мы явно пишем, что это JSON
+    xhr.setRequestHeader("Content-Type", "application/json");
+    // когда придёт ответ на наше обращение к серверу, мы его обработаем здесь
+    xhr.onreadystatechange = function () {
+      // если запрос принят и сервер ответил, что всё в порядке
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // выводим то, что ответил нам сервер — так мы убедимся, что данные он получил правильно
+        getReg(this.responseText);
+      }
+    };
+    // преобразуем наши данные JSON в строку
+    var data = JSON.stringify({ "method": "reg", "name": name.value, "surname": surname.value, "email": email.value, "password": password.value });
+    // когда всё готово, отправляем JSON на сервер
+    xhr.send(data);
+  } else {
+    if (password.value.length <= 8) {
+      notify("error","Ошибка! \nДлина пороля должна быть не менее 8 символов.");
+    }
+    if (password.value != passwordt.value) {
+      notify("error","Ошибка! \nПароли не совпадают!");
+    }
+    if (checkbox.checked == false) {
+      notify("error","Ошибка! \nДля регистрации необходмо дать пользовательское соглашение.");
+    }
+    if (name.value.length <= 2) {
+      notify("error","Ошибка! \nВведите коректное имя.");
+    }
+    if (surname.value.length <= 3) {
+      notify("error","Ошибка! \nВведите коректную фамилию.");
+    }
+    if (vE(email.value) == false) {
+      notify("error","Ошибка! \nВведите коректный e-mail.");
+    }
+  }
+}
+
+
+
+function sendLogin () {
+  let email = document.querySelector('#lemail');
+  let password = document.querySelector('#lpassword');
+  var checkbox = document.getElementById('lcheckbox');  
+
+
+  if (vE(email.value) && password.value.length >= 8){
+    // а вот сюда мы поместим ответ от сервера
+    let result = document.querySelector('.result');
+    // создаём новый экземпляр запроса XHR
+    let xhr = new XMLHttpRequest();
+    // адрес, куда мы отправим нашу JSON-строку
+    let url = myip + "/api";
+    // открываем соединение
+    xhr.open("POST", url, true);
+    // устанавливаем заголовок — выбираем тип контента, который отправится на сервер, в нашем случае мы явно пишем, что это JSON
+    xhr.setRequestHeader("Content-Type", "application/json");
+    // когда придёт ответ на наше обращение к серверу, мы его обработаем здесь
+    xhr.onreadystatechange = function () {
+      // если запрос принят и сервер ответил, что всё в порядке
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // выводим то, что ответил нам сервер — так мы убедимся, что данные он получил правильно
+        getLogin(this.responseText);
+      }
+    };
+    // преобразуем наши данные JSON в строку
+    var data = JSON.stringify({ "method": "login", "email": email.value, "password": password.value, "stay": checkbox.checked});
+    // когда всё готово, отправляем JSON на сервер
+    xhr.send(data);
+  } else {
+    if (vE(email.value) == false) {
+      notify("error","Ошибка! \nВведите коректный e-mail.");
+    }
+    if (password.value.length <= 8) {
+      notify("error","Ошибка! \nВведите коректный пароль.");
+    }
+  }
+}
+
+function getReg (ret) {
+  switch (ret) {
+    case "ANCE":
+      notify("error","Ошибка! \nВведите коректный e-mail.");
+      break;
+    case "ANCP":
+      notify("error","Ошибка! \nВведите коректный пароль.");
+      break;
+    case "ALR":
+      notify("info","Внимание! \nВы уже зарегистрированы!");
+      break;
+    case "ERORR":
+      notify("error","Ошибка!");
+      break;
+    case "OK":
+      window.location.href = myip + '/alreg';
+      break;
+    default:
+      console.log('server return after reg:' + ret);
+      break;
+  }
+}
+
+function getLogin (ret) {
+  switch (ret.split("=")[0]) {
+    case "NO":
+      notify("info","Вы ещё не зарегистрированы.");
+      break;
+    case "PAS":
+      notify("error","Ошибка! \nНеверный пароль!");
+      break;
+    case "ALR":
+      notify("info","Внимание! \nВы уже зарегистрированы!");
+      break;
+    case "ERORR":
+      notify("error","Ошибка!");
+      break;
+    case "id":
+      window.location.href = myip + '/';
+      break;
+    default: 
+      console.log('server return after login:' + ret);
+      break;
+  }
+}
+
