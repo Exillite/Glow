b72efe5b0476c8c292f6a6ef5ba4f9e7f44232d7
Exillite <greencod67@gmail.com>
2022-02-13 16:44:03 +0300

    add exercise page with checkboxes

--- serv.py
+++ serv.py
@@ -257,7 +257,7 @@ def snxpage():
 
 @app.route("/exercise")
 def exercisepage():
-    return render_template('exercies_radio_btn.html', nb=True)
+    return render_template('exercies_checkbox.html', nb=True)
 
 @app.route("/ex_simple_ans")
 def ex_simple_ans():
